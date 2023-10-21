fff7382ef362c72fa7d6907e847fe29f74dd674a
Marketa Belatkova <xbelat02@stud.fit.vutbr.cz>
2023-10-21 17:30:10 +0200

    modified scanner.c, scanner.h using structures and functions from utils.c, utils.h

--- src/scanner.c
+++ src/scanner.c
@@ -1,17 +1,6 @@
 #include "scanner.h"
 #include "errors.h"
-
-void initialize_token(token_t* token) {
-    if (token != NULL) {
-        token->type = TOKEN_ZERO;
-        token->value = NULL;
-        token->next = NULL;
-    }
-}
-
-void clear_buffer(char* buffer) {
-    buffer[0] = '\0';
-}
+#include "utils.h"
 
 bool check_for_keyword(char* text) {
     for(int i = 0; i < NOF_KEYWORDS; i++) {
@@ -30,20 +19,53 @@ bool check_for_datatype(char* text) {
     }
     return false;
 }
+
+void print_Token(TokenT *token){
+    switch(token->type) {
+        case TOKEN_INTEGER:
+            printf("%d\n",token->value.i);
+        case TOKEN_DECIMAL:
+            printf("%f\n",token->value.d);
+        dafault:
+            printf("%s\n",token->value.str);
+    }
+}
+
+void append_and_check(BufferT *buffer, const char ch) {
+    if (buffer_append(buffer, ch) != BUFF_APPEND_SUCCES) {
+        fprintf(stderr, "Internal compiler error. \n");
+        exit(INTERNAL_COMPILER_ERROR);
+    }
+    fprintf(stderr, "%s \n", buffer->bytes);
+}
+
+void error_exit(TokenT *token, BufferT *buffer, char* message, int exit_code) {
+    token_dtor(token); // free(token) bude v token_dtor 
+    buffer_detor(buffer);
+    fprintf(stderr, "%s.\n", message);
+    exit(exit_code);
+}
     
-token_t* generate_token() {
+TokenT* generate_token() {
     FILE* stream = stdin;
-    token_t* token = (token_t*)malloc(sizeof(struct tok)); 
     ScannerState state = STATE_START;
-    char* buffer = malloc(sizeof(char) * MAX_STRLEN); // TREBA POTOM UVOLNIT V PARSERU !!
-    clear_buffer(buffer);
-    int buffer_index = 1;
+    BufferT buffer;
+    if (buffer_init(&buffer) != BUFF_INIT_SUCCES) {
+        fprintf(stderr, "Internal compiler error. \n");
+        exit(INTERNAL_COMPILER_ERROR);
+    }
+    TokenT* token = (TokenT*)malloc(sizeof(TokenT)); // uvolnit v parseru
+    if (token == NULL) {
+        fprintf(stderr, "Internal compiler error. \n");
+        exit(INTERNAL_COMPILER_ERROR);
+    }
+
     int multiline_string_counter = 0;
     bool multiline_string_ok = false;
     // V pripade ze chci vratit charakter do stdin:
     // ungetc(ch, stream);
 
-    initialize_token(token);
+    //token_init(token);
 
     int ch;
     while (true) {
@@ -52,20 +74,25 @@ token_t* generate_token() {
             case STATE_START:
                 if (isalpha(ch) || ch == '_') {
                     state = STATE_TEXT;
-                    sprintf(buffer, "%c", ch);
+                    append_and_check(&buffer, ch);
                 }
                 else if (isspace(ch)) {
                     continue;
                 }
                 else if (ch == '/') {
                     state = STATE_SLASH;
-                    sprintf(buffer, "%c", ch);
+                     append_and_check(&buffer, ch);
                 }
                 else if (ch == '"') {
                     state = STATE_STRING;
                 }
                 else if (ch == EOF) {
-                    token->type = TOKEN_EOF;
+                    append_and_check(&buffer, ch);
+                    token_init(token, TOKEN_EOF, &buffer);
+
+                    print_Token(token);
+                    fprintf(stderr, "TOKEN T%d VALUE %s  (eof) \n", token->type, token->value.str);
+
                     return token;
                 }
                 // ..
@@ -73,15 +100,18 @@ token_t* generate_token() {
 
             case STATE_TEXT:
                 if (isalpha(ch) || ch == '_' || isdigit(ch)) {
-                    buffer[buffer_index++] = ch;
+                    append_and_check(&buffer, ch);
                 } else {
-                    if (check_for_keyword(buffer)) {
-                        token->type = TOKEN_KEYWORD;
+                    if (check_for_keyword(buffer.bytes)) {
+                        token_init(token, TOKEN_KEYWORD, &buffer);
+                        print_Token(token);
+                    } else if(check_for_datatype(buffer.bytes)) {
+                        token_init(token, TOKEN_DATATYPE, &buffer);
                     } else {
-                        token->type = TOKEN_IDENTIFIER;
+                        token_init(token, TOKEN_IDENTIFIER, &buffer);
+                        print_Token(token);
                     }
 
-                    token->value = buffer;
                     return token;
                 }
                 break;
@@ -89,15 +119,14 @@ token_t* generate_token() {
             case STATE_SLASH:
                 if (ch == '/') {
                     state = STATE_LINE_COMMENT;
-                    clear_buffer(buffer);
+                    buffer_clear(&buffer);
                 }
                 else if (ch == '*') {
                     state = STATE_BLOCK_COMMENT;
-                    clear_buffer(buffer);
+                    buffer_clear(&buffer);
                 }
                 else {
-                    token->type = TOKEN_TERM;
-                    token->value = buffer;
+                    token_init(token, TOKEN_TERM, &buffer);
                     return token;
                 }
 
@@ -128,21 +157,15 @@ token_t* generate_token() {
 
             case STATE_STRING:
                 if (ch == '"') {
-                    if (strlen(buffer) == 0) {
+                    if (buffer.length == 0) {
                         state = STATE_TWO_DOUBLE_QUOTES;
                         break;
                     } else {
-                        token->type = TOKEN_STRING;
-                        token->value = buffer;
+                        token_init(token, TOKEN_STRING, &buffer);
                         return token;
                     }
                 }
-
-                if (strlen(buffer) > 0) {
-                    buffer[buffer_index++] = ch;
-                } else {
-                    sprintf(buffer, "%c", ch);
-                }
+                append_and_check(&buffer, ch);
 
                 break;
 
@@ -151,8 +174,7 @@ token_t* generate_token() {
                     state = STATE_MULTILINE_STRING;
                 } else {
                     ungetc(ch, stream);
-                    token->type = TOKEN_STRING;
-                    token->value = buffer;
+                    token_init(token, TOKEN_STRING, &buffer);
                     return token;
                 }
 
@@ -161,25 +183,22 @@ token_t* generate_token() {
             case STATE_MULTILINE_STRING:
                 if (ch == '"') {
                     if (++multiline_string_counter >= 3) {
-                        if (buffer[strlen(buffer)-3] != '\n') {
-                            free(token); free(buffer);
-                            fprintf(stderr, "Lexical error encountered.\n");
-                            exit(LEXICAL_ERROR);
+                        if (buffer.bytes[buffer.length-3] != '\n') {
+                            error_exit(token, &buffer, "Lexical error", LEXICAL_ERROR);
                         }
 
-                        buffer[strlen(buffer)-3] = '\0';
+                        buffer.bytes[buffer.length-3] = '\0';
 
-                        token->type = TOKEN_STRING;
-                        token->value = buffer;
+                        token_init(token, TOKEN_STRING, &buffer);
+                        // token->type = TOKEN_STRING;
+                        // token->value = buffer;
                         return token;
                     }
                 } else {
                     multiline_string_counter = 0;
                     if (!multiline_string_ok) {
                         if (ch != '\n') {
-                            free(token); free(buffer);
-                            fprintf(stderr, "Lexical error encountered.\n");
-                            exit(LEXICAL_ERROR);
+                            error_exit(token, &buffer, "Lexical error", LEXICAL_ERROR);
                         } else {
                             multiline_string_ok = true;
                             break;
@@ -188,13 +207,31 @@ token_t* generate_token() {
 
                 }
                 
-                if (strlen(buffer) > 0) {
-                    buffer[buffer_index++] = ch;
-                } else {
-                    sprintf(buffer, "%c", ch);
-                }
+                append_and_check(&buffer, ch);
+                // if (buffer.length > 0) {
+                //     buffer[buffer_index++] = ch;
+                // } else {
+                //     sprintf(buffer, "%c", ch);
+                // }
 
                 break;
         }
     }
+}
+
+int main() {
+    TokenT* token = NULL;
+    while (true) {
+        token = generate_token();
+        if (token->type == TOKEN_EOF) {
+            break;
+        }
+
+        printf("TOKEN T%d VALUE %s\n", token->type, token->value.str);
+
+        token_dtor(token);
+        free(token);
+    }
+
+    return 0;
 }
--- src/scanner.h
+++ src/scanner.h
@@ -7,20 +7,6 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
-typedef enum tokentype_e {
-    TOKEN_ZERO, // lze na nej inicializovat token
-    TOKEN_IDENTIFIER,
-    TOKEN_KEYWORD,
-    TOKEN_DATATYPE,
-    TOKEN_STRING, 
-    TOKEN_INTEGER, 
-    TOKEN_DECIMAL,
-    TOKEN_TERM,
-    TOKEN_LINE_COMMENT,
-    TOKEN_BLOCK_COMMENT,
-    TOKEN_EOF
-} TokenType;
-
 typedef enum fsm_state_e {
     STATE_START,
     STATE_TEXT,
@@ -32,12 +18,6 @@ typedef enum fsm_state_e {
     STATE_MULTILINE_STRING,
     STATE_TWO_DOUBLE_QUOTES
 } ScannerState;
-
-typedef struct tok {
-    TokenType type;
-    char* value;
-    struct token_t* next;
-} token_t;
 
 #define MAX_STRLEN 255 // NENI OMEZENA, VYRESIT !!
 #define NOF_KEYWORDS 8
