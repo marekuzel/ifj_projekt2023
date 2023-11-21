38aa6b3c3b81d6619d81d42df67b251b0bd6c1b5
timinka <adamcikovatimea@gmail.com>
2023-11-21 16:42:14 +0100

    stack to utils

--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,10 @@
 #include <stdbool.h>
-
+#include "errors.h"
 #ifndef UTILS_H
 #define UTILS_H
+
+
+
 
 /**
  * @brief Buffet ADT implementation
@@ -219,6 +222,27 @@ typedef enum rel_op {
 } rel_op_t;
 
 
+#define MAXSTACK 30
+
+#define STACKDEC(T, TNAME)                                                     \
+  typedef struct {                                                             \
+    T items[MAXSTACK];                                                         \
+    int top;                                                                   \
+  } stack_##TNAME##_t;                                                         \
+                                                                               \
+  void stack_##TNAME##_init(stack_##TNAME##_t *stack);                         \
+  void stack_##TNAME##_push(stack_##TNAME##_t *stack, T item);                 \
+  T stack_##TNAME##_pop(stack_##TNAME##_t *stack);                             \
+  T stack_##TNAME##_top(stack_##TNAME##_t *stack);                             \
+  bool stack_##TNAME##_empty(stack_##TNAME##_t *stack);                        \
+
+STACKDEC(char*, char)
+
+bool stack_char_2oftop(stack_char_t *stack);
+Error stack_insertAfterTerminal(stack_char_t* stack);
+int stack_numOfElem(stack_char_t* stack);
+void stack_topTerminal(stack_char_t* stack, char **term);
+//void print_stack(stack_char_t* stack);
 
 
 #endif
