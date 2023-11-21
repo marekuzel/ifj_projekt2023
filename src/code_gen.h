7c4758cdfdedd7b6d4fd1aa993f610690ce897cd
Tomas-Zgut <xzgutt00@stud.fit.vutbr.cz>
2023-11-21 11:31:23 +0100

    ast is done as an ADT, code gen needs scopes and function calls

--- src/code_gen.h
+++ src/code_gen.h
@@ -24,3 +24,17 @@ void gen_cond(ast_node_t *ast, const char *label_name, const int label_num);
 
 
 void gen_statement(ast_node_t *ast);
+
+void gen_string_op(ast_node_t *ast);
+
+void gen_write(ast_node_t *ast);
+
+void gen_read(ast_node_t *ast, const char *type);
+
+void gen_statements(ast_node_t *ast);
+
+void gen_loop(ast_node_t *ast);
+
+void gen_cond_branch(ast_node_t *ast);
+
+void gen_function_call(ast_node_t *ast);
