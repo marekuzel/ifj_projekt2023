e2f21d96c39fbf9c76583e7e04f536c1674ac8b8
marekuzel <marekuzel1@gmail.com>
2023-11-23 00:14:31 +0100

    Return values of rules, struct Parser
    Added proper return values of syntax rules, changed them so they always
    return some value.
    Also added parser->symtable and workaround aorund the issue of expr rule
    getting more tokens then it should

--- src/parser.c
+++ src/parser.c
@@ -11,19 +11,34 @@ void Parser_init(Parser_t *parser){
     parser->token_topOfStack = NULL;
     Stack *stack = malloc(sizeof(Stack));
     Stack_Init(stack);
+    symtable_t *symtable = malloc(sizeof(symtable_t));
+    table_init(symtable);
 }
 
-void Parser_getNewToken(Parser_t *parser){
-    Stack *stack = parser->stack;
-    Stack_Push(stack, parser->token_current);
+void parser_stashExtraToken(Parser_t *parser, TokenT *token){
+    parser->token_extraToken = token;
+}
+
+void parser_getNewToken(Parser_t *parser){
+    Stack_Push(parser->stack, parser->token_current);
     parser->token_topOfStack = parser->token_current;
-    parser->token_current = generate_token();
+    if (parser->token_extraToken != NULL){
+        parser->token_current = parser->token_extraToken;
+        parser->token_extraToken = NULL;
+    }
+    else{
+        parser->token_current = generate_token();
+}
 }
 
-parser_ret_t Parser_dtor(Parser_t * parser){
-    if (parser == NULL)
-        return PARSER_DTOR_FAIL;
+void parser_dtor(Parser_t * parser){
+    table_dispose(parser->symtable);
     Stack_Dispose(parser->stack);
     free(parser);
-    return PARSER_DTOR_SUCCES;
 }
+
+Error parser_addLocalSymtable(Parser_t *parser){
+    table_add_scope(parser->symtable);
+    return SUCCESS;
+}
+
