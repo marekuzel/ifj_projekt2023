ff1a41218bfbcfc7ef664f03b0572e29cd0d2ea7
Marketa Belatkova <xbelat02@stud.fit.vutbr.cz>
2023-12-06 23:34:00 +0100

    added comments

--- src/scanner.h
+++ src/scanner.h
@@ -38,6 +38,52 @@ typedef enum fsm_state_e {
 #define NOF_DATATYPES 3
 #define MAX_DTT_KWD_LEN 10
 
+/**
+ * @brief function checking if returning token matches with some of keywords
+ * @param text: checked value of returning token
+ * @return true if value of token matches with some of keywords
+ * @return false otherwise
+ */
+bool check_for_keyword(char* text);
+
+/**
+ * @brief function checking if returning token matches with some of datatypes
+ * @param text: checked value of returning token
+ * @return true if value of token matches with some of datatypes
+ * @return false otherwise
+ */
+bool check_for_datatype(char* text);
+
+/**
+ * @brief function searching for keywords' token_type matches with token value
+ * @param keyword: value of returning token
+ * @return token type matching with right keyword
+ * @return token type TOKEN_ZERO otherwise
+ */
+TokenType keyword_to_token(char* keyword);
+
+/**
+ * @brief function searching for datatypes' token_type matches with token value
+ * @param datatype: value of returning token
+ * @return token type matching with right datatype
+ * @return token type TOKEN_ZERO otherwise
+ */
+TokenType datatype_to_token(char* datatype);
+
+/**
+ * @brief function checking type and validity of escape sequence
+ * @param buffer: buffer of characters corresponding to the token value
+ * @param ch: character specifying the type of escape sequence
+ * @return STATE_START if invalid escape sequence was given or if sequence appending to buffer failed
+ * @return STATE_ESCAPE if sequence "\u" was given
+ * @return STATE_STRING otherwise
+ */
+ScannerState handle_escape_sequence(BufferT* buffer, char ch);
+
+/**
+ * @brief function generating token and checking for lexical errors based on Final State Machine
+ * @return token structure with token type and value, terminate with error otherwise
+ */
 TokenT *generate_token();
 
 #endif
